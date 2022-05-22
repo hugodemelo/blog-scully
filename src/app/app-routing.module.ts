09af1cb60388cee301199b5128c75dadb8cada29
Hugo de Melo <hsemog@gmail.com>
2022-05-22 13:34:24 +0200

    articles page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: '', redirectTo: 'articles', pathMatch: 'full'},
+  { path: '**', redirectTo: 'articles' }
 ];
 
 @NgModule({
